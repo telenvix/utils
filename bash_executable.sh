5ee5828646e58d5b9fedf8b8901605abca15f132
serdar <101868505+telenvix@users.noreply.github.com>
2022-04-19 17:13:26 +0200

    Create bash_executable.sh
    Exists

---
+++ bash_executable.sh
@@ -0,0 +1,7 @@
+# Definir chemin vers python voulu
+# Ajouter chemin complet vers le script a lancer
+# Ajouter les arguments
+# Rendre le fichier executable avec terminal : chmod +x bash_executable.sh
+
+#!/conda/geo_py37/bin/python
+/usr/bin/python script.py --arg1 --arg2 --arg3 --arg4 --arg5
